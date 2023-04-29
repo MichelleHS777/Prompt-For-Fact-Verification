ff8686d953e41621ad637e440c4639301dd1a5c6
michelle316 <38562417+MichelleHS777@users.noreply.github.com>
2023-04-29 23:26:54 +0800

    230429

---
+++ src/run.sh
@@ -0,0 +1,5 @@
+python prefix.py > result/prefixtuning_1.log
+python prefix.py > result/prefixtuning_2.log
+python prefix.py > result/prefixtuning_3.log
+python prefix.py > result/prefixtuning_4.log
+python prefix.py > result/prefixtuning_5.log
