c7c8b5b7f9bc0b61b7932b4a0816177de0ed716c
michelle316 <38562417+MichelleHS777@users.noreply.github.com>
2023-07-27 22:28:48 +0800

    Add files via upload

--- preprocess.py
+++ preprocess.py
@@ -1,8 +1,8 @@
 from tqdm import tqdm
 import json
 
-dataset = json.load(open('./datasets/unpreprocess/train.json', 'r', encoding='utf-8'))
-save_file = open('datasets/preprocessed/train2.json', 'w', encoding='utf-8')
+dataset = json.load(open('./datasets/unpreprocess/test.json', 'r', encoding='utf-8'))
+save_file = open('datasets/preprocessed/test2.json', 'w', encoding='utf-8')
 
 
 for data in tqdm(dataset, desc='Preprocess...'):
@@ -11,11 +11,6 @@ for data in tqdm(dataset, desc='Preprocess...'):
     evidences = [data['gold evidence'][str(i)]['text'] for i in range(5)]
     evidences = ''.join(evidences)
     label = data['label']
-    if label == 2:
-        label2 = 0
-    else:
-        label2 = 1
-    data = json.dumps({'claimId': claimId, 'claim': claim, 'evidences': evidences, 'label':label, 'label2':label2}, ensure_ascii=False)
-
+    data = json.dumps({'claimId': claimId, 'claim': claim, 'evidences': evidences, 'label': label}, ensure_ascii=False)
     save_file.write(data + "\n")
 save_file.close()
