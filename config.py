db307d63a96acd3bb70ac04b11cabaf190a61fb1
michelle316 <38562417+MichelleHS777@users.noreply.github.com>
2023-07-27 22:14:31 +0800

    Update config.py

--- config.py
+++ config.py
@@ -3,12 +3,12 @@ import argparse
 
 def set_args():
     parser = argparse.ArgumentParser(description='Prompt Tuning For CHEF')
-    parser.add_argument('--cuda', type=str, default="0", help='appoint GPU devices')
-    parser.add_argument('--data_aug', action="store_true", help='If data augmentation or not')
+    # parser.add_argument('--cuda', type=str, default="0", help='appoint GPU devices')
+    # parser.add_argument('--data_aug', action="store_true", help='If data augmentation or not')
     parser.add_argument('--train', action="store_true", help='If train or not')
     parser.add_argument('--eval', action="store_true", help='If evaluate or not')
     parser.add_argument('--train_file', type=str, default='datasets/preprocessed/train.json', help='train dataset path')
-    parser.add_argument('--fake_train_file', type=str, default='datasets/preprocessed/train_fake_SUP_NE_1000.json', help='fake nei dataset for training')
+    # parser.add_argument('--fake_train_file', type=str, default='datasets/preprocessed/train_fake_SUP_NE_1000.json', help='fake nei dataset for training')
     parser.add_argument('--valid_file', type=str, default='datasets/preprocessed/dev.json', help='validation dataset path')
     parser.add_argument('--test_file', type=str, default='datasets/preprocessed/test.json', help='test dataset path')
     parser.add_argument('--plm', type=str, default='bert', help='bert, roberta, ernie')
@@ -26,10 +26,9 @@ def set_args():
     parser.add_argument('--soft_token_num', type=int, default=10, help='set the number of soft token')
     parser.add_argument('--plm_eval_mode', action="store_true", help='the dropout of the model is turned off')
     parser.add_argument("--template", type=int,
-                        help="Set template (0 for manual, 1 for soft, 2 for Ptuning, 3 for PrefixTuning, 4 for PTR, 5 for mix)",
+                        help="Set template (0 for manual, 1 for Ptuning (soft APE), 2 for Ptuning (soft: 10 + APE), 3 for PTuning (soft: 10))",
                         default=0)
-    parser.add_argument("--verbalizer", type=int,
-                        help="Set template (0 for manual, 1 for soft, 2 for knowledge)", default=0)
-    parser.add_argument("--init_from_vocab", action="store_false")
+    parser.add_argument("--verbalizer", type=int, help="Set manual template", default=0)
+    # parser.add_argument("--init_from_vocab", action="store_false")
     parser.add_argument('--model_save_dir', type=str, default='./checkpoint/', help='save model path')
     return parser.parse_args()
