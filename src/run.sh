41c53ec9d09ba24a82f8a5d15d2b74597e2b6e29
michelle316 <38562417+MichelleHS777@users.noreply.github.com>
2023-07-27 22:15:23 +0800

    Delete src directory

--- src/run.sh
+++
@@ -1,5 +0,0 @@
-python prefix.py > result/prefixtuning_1.log
-python prefix.py > result/prefixtuning_2.log
-python prefix.py > result/prefixtuning_3.log
-python prefix.py > result/prefixtuning_4.log
-python prefix.py > result/prefixtuning_5.log
