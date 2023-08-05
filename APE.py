c3ddc8622fb9483ac65495df15a683615785a39d
michelle316 <38562417+MichelleHS777@users.noreply.github.com>
2023-08-05 12:30:41 +0800

    Update APE.py

--- APE.py
+++ APE.py
@@ -1,14 +1,17 @@
 import openai
 import json
 
-
-openai.api_key = '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'
+# ------------------------init parameters----------------------------
+parser = argparse.ArgumentParser(description='P-tuning v2')
+parser.add_argument('--dataset', type=str, default="./datasets/train.json", help='dataset path')
+args = parser.parse_args()
+# set dataset path
 dataset = json.load(open('./datasets/train.json', 'r', encoding='utf-8'))
+# set openai api key
+openai.api_key = 'KEY'
 
 # First, let's define a simple dataset consisting of claim and label
-
 label_mapping = {0:'支持',1:'反对',2:'信息不足'}
-
 claim_evidence = []
 for i in range(len(dataset)):
     claim = dataset[i]['claim']
@@ -19,32 +22,30 @@ for i in range(len(dataset)):
 label = [label_mapping[dataset[i]['label']] for i in range(20)]
 
 # Now, we need to define the format of the prompt that we are using.
-
 eval_template = \
 """Instruction: [PROMPT]
 Input: [INPUT]
 Output: [OUTPUT]"""
 
-# # Now, let's use APE to find prompts that generate antonyms for each word.
-# from automatic_prompt_engineer import ape
-#
-# result, demo_fn = ape.simple_ape(
-#     dataset=(claim_evidence, label),
-#     eval_template=eval_template,
-# )
-#
-# # Let's see the results.
-# print(result)
-
-
+# Now, let's use APE to find prompts that generate antonyms for each word.
 from automatic_prompt_engineer import ape
 
-manual_prompt = "要評估輸入和證據的結果是支持或反對，或者信息不足"
-
-human_result = ape.simple_eval(
+result, demo_fn = ape.simple_ape(
     dataset=(claim_evidence, label),
     eval_template=eval_template,
-    prompts=[manual_prompt],
 )
 
-print(human_result)
+# Let's see the results.
+print(result)
+
+# if you want to evalauate the score of Prompts
+# from automatic_prompt_engineer import ape
+
+# manual_prompt = "要評估輸入和證據的結果是支持或反對，或者信息不足"
+
+# human_result = ape.simple_eval(
+#     dataset=(claim_evidence, label),
+#     eval_template=eval_template,
+#     prompts=[manual_prompt],
+# )
+# print(human_result)
